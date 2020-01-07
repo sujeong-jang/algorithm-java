3c61081bba847a683ecabc1bc317d0065b501ccd
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2020-01-07 22:58:09 +0900

    문제1463_1로 만들기

---
+++ src/baekjoon_algorithm/Num1463.java
@@ -0,0 +1,28 @@
+package baekjoon_algorithm;
+
+import java.util.Scanner;
+
+public class Num1463 {
+
+	public static void go(int n) {
+		int a[] = new int[n+1];
+		a[1] = 0;
+
+		for (int i=2; i <= n; i++) {
+			a[i] = a[i-1] + 1;
+			if (i % 2 == 0)
+				a[i] = Math.min(a[i], a[i/2] + 1);
+			if (i % 3 == 0)
+				a[i] = Math.min(a[i], a[i/3] + 1);
+		}
+		System.out.println(a[n]);
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		go(n);
+
+	}
+}
