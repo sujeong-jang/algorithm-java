814875469cd10868958a23c284ce38071a14b441
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2020-01-11 00:49:30 +0900

    Add problem description

--- src/dp/Num11726.java
+++ src/dp/Num11726.java
@@ -1,7 +1,10 @@
 package dp;
 
 import java.util.Scanner;
-
+/*
+ * 2xn 타일링
+ * 2×n 크기의 직사각형을 1×2, 2×1 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+ */
 public class Num11726 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
