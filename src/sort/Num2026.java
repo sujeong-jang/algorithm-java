fb681dbcd7cd90161e43b4f828b3471179d08fa6
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2020-02-09 23:13:56 +0900

    문제1026_보물

---
+++ src/sort/Num2026.java
@@ -0,0 +1,35 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Num2026 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int[] a = new int[n];
+		Integer[] b = new Integer[n];
+		
+		for(int i=0; i<n; i++) {
+			a[i] = s.nextInt();
+		}
+		
+		for(int i=0; i<n; i++) {
+			b[i] = s.nextInt();
+		}
+
+		Arrays.sort(a);
+		Arrays.sort(b, Collections.reverseOrder());
+		
+		int sum = 0;
+		for(int i=0; i<n; i++) {
+			sum += a[i] * b[i];
+		}
+		
+		System.out.println(sum);
+		
+	}
+
+}
