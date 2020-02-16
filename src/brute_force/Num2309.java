da667b2ca1b787a4ad528d306c7e6af79c627e9f
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2020-02-16 16:32:44 +0900

    문제2309_일곱난쟁이

---
+++ src/brute_force/Num2309.java
@@ -0,0 +1,42 @@
+package brute_force;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+ * 일곱 난쟁이
+ * 왕비를 피해 일곱 난쟁이들과 함께 평화롭게 생활하고 있던 백설공주에게 위기가 찾아왔다. 
+ * 일과를 마치고 돌아온 난쟁이가 일곱 명이 아닌 아홉 명이었던 것이다.
+ * 아홉 명의 난쟁이는 모두 자신이 "백설 공주와 일곱 난쟁이"의 주인공이라고 주장했다.
+ * 뛰어난 수학적 직관력을 가지고 있던 백설공주는, 다행스럽게도 일곱 난쟁이의 키의 합이 100이 됨을 기억해 냈다.
+ * 아홉 난쟁이의 키가 주어졌을 때, 백설공주를 도와 일곱 난쟁이를 찾는 프로그램을 작성하시오.
+ */
+public class Num2309 {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+        int n = 9;
+        int q = 0;
+        int w = 0;
+        int[] a = new int[n];
+        int sum = 0;
+        for (int i=0; i<n; i++) {
+            a[i] = sc.nextInt();
+            sum += a[i];
+        }
+        Arrays.sort(a);
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                if (sum - a[i] - a[j] == 100) {
+                    q = i;
+                    w = j;
+                }
+            }
+        }
+        
+        for(int i=0; i<n; i++) {
+        	if(i == q || i == w) continue;
+        	System.out.println(a[i]);
+        }
+    }
+}
